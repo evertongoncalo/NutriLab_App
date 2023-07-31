ec65d03afcdbd85325523474ea3acd14689b6c57
Everton <everton.aga88@gmail.com>
2023-07-31 20:01:19 +0100

    up

--- authentication/views.py
+++ authentication/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .utils import *
 
 
 
 def register(request):
-    return render(request, 'register.html')
+    if request.method == 'POST':
+        usuario = request.POST.get('usuario')
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
+        confirmar_senha = request.POST.get('confirmar_senha')
+        if not password_is_valid(request, senha,confirmar_senha):
+            return redirect('/auth/register')
+        
+    elif request.method == 'GET':
+        return render(request, 'register.html')
 
 def login(request):
     return render(request, 'login.html')
