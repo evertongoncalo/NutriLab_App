413d2f1a7a5bd6452171246826f073493d65df6c
Everton <everton.aga88@gmail.com>
2023-08-01 08:30:14 +0100

    up

--- authentication/views.py
+++ authentication/views.py
@@ -1,20 +1,55 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .utils import *
+from django.contrib.auth.models import User
+from django.contrib.messages import constants
+from django.contrib import messages,auth
 
 
 
 def register(request):
-    if request.method == 'POST':
+    if request.method == 'GET':
+        return render(request, 'register.html')
+    
+    elif request.method == 'POST':
         usuario = request.POST.get('usuario')
         email = request.POST.get('email')
         senha = request.POST.get('senha')
         confirmar_senha = request.POST.get('confirmar_senha')
         if not password_is_valid(request, senha,confirmar_senha):
             return redirect('/auth/register')
+        try:
+            user = User.objects.create_user(username=usuario,email=email,password=senha,is_active=False) #is active é para o usuario receber o email pra confirmar e depois ai sim poder acessar o sistema.
+            print(user)
+            user.save()
+            messages.add_message(request,constants.SUCCESS, "Usuario cadastrado")
+            return redirect('/auth/login')
+        except:
+            messages.add_message(request,constants.ERROR, "Erro do sistema")
+            return redirect('/auth/register')
         
-    elif request.method == 'GET':
-        return render(request, 'register.html')
+        
+def logar(request):
+    if request.method == "GET":
+        if request.user.is_authenticated:
+            return redirect('/')
+        return render(request, 'login.html')
+    elif request.method == "POST":
+        username = request.POST.get('usuario')
+        senha = request.POST.get('senha')
+
+        usuario = auth.authenticate(username=username, password=senha)
+
+        if not usuario:
+            messages.add_message(request, constants.ERROR, 'Username ou senha inválidos')
+            return redirect('/auth/login')
+        else:
+            auth.login(request, usuario)
+            return redirect('/')    
 
 def login(request):
     return render(request, 'login.html')
+
+def sair(request):
+    auth.logout(request)
+    return redirect('/auth/login')
--- nutrilab/settings.py
+++ nutrilab/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from django.contrib.messages import constants
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -128,3 +129,17 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+MESSAGE_TAGS = {
+    constants.DEBUG: 'alert-primary',
+    constants.ERROR: 'alert-danger',
+    constants.SUCCESS: 'alert-success',
+    constants.INFO: 'alert-info',
+    constants.WARNING: 'alert-warning',
+}
+
+# Email
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_HOST_USER = "everton.aga88@gmail.com"
